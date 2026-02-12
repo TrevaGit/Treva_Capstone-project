f631606f86c9beff32abb9de4cc62cd48a2dcea4
Treva Onyango Aoko <trevaaoko1235@gmail.com>
2026-02-13 01:23:37 +0300

    Update TaskList.jsx

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import TaskItem from "./TaskItem";
 
-const TaskList = () => {
-  return <div className="p-4">Task List Placeholder</div>;
+const TaskList = ({ tasks, toggleComplete, deleteTask }) => {
+  if (tasks.length === 0) {
+    return <p className="text-gray-500 text-center">No tasks yet</p>;
+  }
+
+  return (
+    <ul className="space-y-2">
+      {tasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          toggleComplete={toggleComplete}
+          deleteTask={deleteTask}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default TaskList;
