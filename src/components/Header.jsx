d7838242ac1769a6c1e504a97c2eecc70661cf56
Treva Onyango Aoko <trevaaoko1235@gmail.com>
2026-02-13 01:17:20 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
+import React from "react";
 
+const Header = () => {
+  return (
+    <header className="bg-blue-500 text-white text-center p-4 text-2xl font-bold">
+      To-Do List App
+    </header>
+  );
+};
+
+export default Header;
